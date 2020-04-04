484497d3f8659a4edab2f6b3b7197c09ed16de6b
Phan Manh Tien <manhtien@gmail.com>
2020-04-04 21:50:01 +0700

    Add module chalk and update something for Dog.js

--- Dog.js
+++ Dog.js
@@ -1,3 +1,11 @@
-function Dog() {
-	
+var chalk = require('chalk');
+
+function Dog(name) {
+	this.name = name
 }
+
+Dog.prototype.sayHi = function() {
+	console.log('Hi, I am a dog, my name is ' +  chalk.green(this.name));
+};
+
+module.exports = Dog;
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var meo = require('./Cat.js');
 var chuot = require('./Mouse.js');
+var cho = require('./Dog.js')
 
 var mouse1 = new chuot('Jerry');
 var cat1 = new meo('Tom');
 var mouse2 = new chuot('Mickey');
+var dog1 = new cho('Milu');
 
+dog1.sayHi();
 cat1.eat(mouse1);
 cat1.eat(mouse2);
 
-console.log(cat1, mouse1, mouse2);
+//console.log(cat1, mouse1, mouse2);
