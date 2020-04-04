d1c56ccb7baf67bdaa771d65f8cafae2b903a245
Phan Manh Tien <manhtien@gmail.com>
2020-04-04 21:26:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var chuot = require('./Mouse.js');
 
 var mouse1 = new chuot('Jerry');
 var cat1 = new meo('Tom');
+var mouse2 = new chuot('Mickey');
 
 cat1.eat(mouse1);
+cat1.eat(mouse2);
 
-console.log(cat1, mouse1);
+console.log(cat1, mouse1, mouse2);
