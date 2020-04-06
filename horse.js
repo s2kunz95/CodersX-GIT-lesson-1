cad2a6a9ab8c227291208bacf69213146f95d2dd
Manh Tien <excier260@gmail.com>
2020-04-06 22:23:51 +0700

    Add horse jump method

--- horse.js
+++ horse.js
@@ -2,8 +2,8 @@ function Horse() {
 
 }
 
-Horse.prototype.run =  function(){
-	console.log('Running...')
+Horse.prototype.jump =  function(){
+	console.log('Jumping...')
 }
 
 module.exports = Horse;
